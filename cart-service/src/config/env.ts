67af52e92cfe77a08c8fd7fa5be6a009153e80fa
abhishek <abhiabhishektr@gmail.com>
2024-10-13 11:58:34 +0530

    deployment code added

--- cart-service/src/config/env.ts
+++ cart-service/src/config/env.ts
@@ -1,5 +1,5 @@
 export const env = {
-    PORT: process.env.PORT || 3002,
-    SERVICE: process.env.SERVICE || 'cart-service',
-    KAFKA_BROKER_URL: process.env.KAFKA_BROKER_URL || 'localhost:9092',
+    PORT: 3002,
+    SERVICE:  'cart-service',
+    KAFKA_BROKER_URL:  'localhost:9092',
 };
